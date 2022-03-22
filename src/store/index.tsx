c70aa5fc251e94e9a3f1d67168e22c84bdc53888
Marc Schulmeister <marcschulm@gmail.com>
2022-03-21 22:22:16 -0700

    update

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,7 +1,4 @@
-import {
-  createStore,
-  applyMiddleware
-} from "redux";
+import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 import gameReducer from "./reducers";
 import watcherSagas from "./sagas";
--- src/store/reducers/index.tsx
+++ src/store/reducers/index.tsx
@@ -1,20 +1,92 @@
-// Reducers are functions that return a new global state every time an action is dispatched.
+import {
+  DOWN,
+  INCREASE_SNAKE,
+  INCREMENT_SCORE,
+  ISnakeCoord,
+  LEFT,
+  RESET,
+  RESET_SCORE,
+  RIGHT,
+  SET_DIS_DIRECTION,
+  UP,
+} from "../actions";
 
-const GlobalState = {
-  data: ""
+export interface IGlobalState {
+  snake: ISnakeCoord[] | [];
+  disallowedDirection: string;
+  score: number;
+}
+
+const globalState: IGlobalState = {
+  snake: [
+    { x: 580, y: 300 },
+    { x: 560, y: 300 },
+    { x: 540, y: 300 },
+    { x: 520, y: 300 },
+    { x: 500, y: 300 },
+  ],
+  disallowedDirection: "",
+  score: 0,
+};
+const gameReducer = (state = globalState, action: any) => {
+  switch (action.type) {
+    case RIGHT:
+    case LEFT:
+    case UP:
+    case DOWN: {
+      let newSnake = [...state.snake];
+      newSnake = [{
+        x: state.snake[0].x + action.payload[0],
+        y: state.snake[0].y + action.payload[1],
+      }, ...newSnake];
+      newSnake.pop();
+
+      return {
+        ...state,
+        snake: newSnake,
+      };
+    }
+
+    case SET_DIS_DIRECTION:
+      return { ...state, disallowedDirection: action.payload };
+
+    case RESET:
+      return {
+        ...state,
+        snake: [
+          { x: 580, y: 300 },
+          { x: 560, y: 300 },
+          { x: 540, y: 300 },
+          { x: 520, y: 300 },
+          { x: 500, y: 300 },
+        ],
+        disallowedDirection: ""
+      };
+
+    case INCREASE_SNAKE:
+      const snakeLen = state.snake.length;
+      return {
+        ...state,
+        snake: [
+          ...state.snake,
+          {
+            x: state.snake[snakeLen - 1].x - 20,
+            y: state.snake[snakeLen - 1].y - 20,
+          },
+        ],
+      };
+
+    case RESET_SCORE:
+      return { ...state, score: 0 };
+
+    case INCREMENT_SCORE:
+      return {
+        ...state,
+        score: state.score + 1,
+      };
+    default:
+      return state;
+  }
 };
 
-const gameReducer = (state = GlobalState, action) => {
-  switch (action.type) {
-      case "MOVE_RIGHT":
-          /**
-           * Perform a certain set of operations
-           */
-          return {
-              ...state, data: action.payload
-          };
-
-      default:
-          return state;
-  }
-}
+export default gameReducer;
